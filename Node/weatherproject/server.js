7b9925bf0120ea238894b737305547abf2e7eafa
pranay2401 <pranay.bajaj92@gmail.com>
2020-12-09 00:09:17 +0530

    Add files via upload

---
+++ Node/weatherproject/server.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const https = require("https") // No need to install. It is native
+const bodyParser = require("body-parser")
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.post("/", function (req, resp) {
+  const city = req.body.city
+  if (city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=3059eb7e239cbbd23838b9dde7396dec`
+
+    https.get(url, function(response) {
+      console.log(response.statusCode)
+      response.on("data", function(data) {
+        dataJSON = JSON.parse(data)
+        console.log(dataJSON);
+
+        const temp = dataJSON.main.temp
+        const weatherDescription = dataJSON.weather[0].description
+        const iconCode = dataJSON.weather[0].icon
+        const iconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`
+
+        // for multiple sending, we use write function
+        resp.write("<h1>The weather condition at " + city + " is : " + weatherDescription + "<img src=" + iconURL + "></img>" + "</h1>")
+        resp.write("The temperature is " + temp + " degree Celcius")
+        resp.send()
+      })
+    })
+  }
+})
+
+app.get("/", function(req, resp) {
+  resp.sendFile(__dirname + "/index.html")
+})
+
+app.listen(3000, function() {
+  console.log("Weather App running on port 3000");
+})
